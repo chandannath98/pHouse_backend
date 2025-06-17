3610ebb4aff8b57633e1656e10f680fb7566f01b
@chandan.nath <chandan.nath@sharajman.com>
2025-06-17 12:51:04 +0530

    login admin added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+const Admin = require("../models/Admin");
+
 const router = express.Router();
 
 // Register User
@@ -21,6 +23,26 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+
+// Register User
+router.post("/registeradmin", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await Admin.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const newUser = new Admin({ name, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
 // Login User
 router.post("/login", async (req, res) => {
   try {
@@ -38,5 +60,29 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+
+
+// Login User
+router.post("/login_admin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Admin.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
+
 
 module.exports = router;
